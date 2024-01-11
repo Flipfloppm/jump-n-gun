51419107068fb52b005417dcee7e5edac9a2d07c
Flipfloppm <franklin.s.dai@gmail.com>
2024-01-10 19:31:48 -0600

    added gun rotation and bullet firing co-author: ywiamnog

--- Characters/player.gd
+++ Characters/player.gd
@@ -1,12 +1,16 @@
 extends CharacterBody2D
-
+signal pickedUp
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var hasPistol = false
+@export var bullet :PackedScene
 
+func _ready():
+	$GunRotation/Pistol.visible = false
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -24,10 +28,25 @@ func _physics_process(delta):
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		
+	# This rotates the gun following the mouse
+	$GunRotation.look_at(get_viewport().get_mouse_position())
+	if Input.is_action_just_pressed("shoot") and hasPistol:
+		var b = bullet.instantiate()
+		b.global_position = $GunRotation/BulletSpawn.global_position
+		b.rotation_degrees = $GunRotation.rotation_degrees
+		get_tree().root.add_child(b)
+		
+		
 	move_and_slide()
+	
+	
 
 
 func _on_pistol_body_entered(body):
-	
 	print("entered")
+	#hide()
+	pickedUp.emit()
+	hasPistol = true
+	$GunRotation/Pistol.visible = true
+	#$CollisionShape2D.set_deferred("disabled", true)
