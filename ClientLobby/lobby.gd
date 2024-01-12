6bcbc25e6b8bf297397aa0b72d200dda4dd8479b
Davids048 <90978028+Davids048@users.noreply.github.com>
2024-01-12 16:16:19 -0600

    Make tutorial scene auto spawn 2 players They are not synced yet

--- ClientLobby/lobby.gd
+++ ClientLobby/lobby.gd
@@ -1,6 +1,6 @@
 extends Control
 
-@onready var server = $Server
+@onready var server = $CenterContainer/VBoxContainer/Server
 @onready var player_name = $CenterContainer/VBoxContainer/GridContainer/NameEdit
 @onready var selected_IP = $CenterContainer/VBoxContainer/GridContainer/IPEdit
 @onready var selected_port = $CenterContainer/VBoxContainer/GridContainer/PortEdit
@@ -15,7 +15,7 @@ func _ready():
 func connected_to_server():
 	print("connected to server")
 	# call register info on the server first
-	#register_player_info.rpc_id(1, $PlayerNameTextBox.text, multiplayer.get_unique_id())
+	register_player_info.rpc_id(1, player_name.text, multiplayer.get_unique_id())
 
 # this gets called only by clients 
 func connection_failed():
@@ -26,6 +26,7 @@ func _on_host_game_pressed():
 	selected_port.text = str(host_info[0])
 	selected_IP.text = str(host_info[1])
 	joinBtn.disabled = true
+	register_player_info(player_name.text, multiplayer.get_unique_id())
 	
 func _on_join_btn_pressed():
 	print("join pressed")
@@ -38,10 +39,25 @@ func _on_start_game_btn_pressed():
 
 @rpc("any_peer", "call_local")
 func start_game():
-	var start_scene = load("res://Levels/test_level.tscn").instantiate()
+	var start_scene = load("res://Levels/tutorial_level.tscn").instantiate()
 	get_tree().root.add_child(start_scene)
 	print("started game" + str(multiplayer.get_unique_id()))
 	self.hide()
+	
+@rpc("any_peer")
+func register_player_info(player_name, id):
+	if not GameManager.PLAYERS.has(id):
+		GameManager.PLAYERS[id] = {
+			"name":player_name,
+			"id":id,
+			"score": 0
+		}
+	# after the server received the connected player's info, the server broadcast
+	# the info to the other players.
+	if multiplayer.is_server():
+		for playerID in GameManager.PLAYERS:
+			register_player_info.rpc(GameManager.PLAYERS[playerID].name, playerID)
+
 
 
 
