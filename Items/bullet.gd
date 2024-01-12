5f059050ffaf0a0ac467a928b77ded91ae10c91d
Flipfloppm <franklin.s.dai@gmail.com>
2024-01-11 19:37:07 -0600

    fixed logic for collisions and deleting the bullet

--- Items/bullet.gd
+++ Items/bullet.gd
@@ -1,20 +1,28 @@
 extends CharacterBody2D
 
 
-const SPEED = 500.0
+const SPEED = 800.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var direction : Vector2
+var collision: KinematicCollision2D
+var collided = false
+var collisionVelocity: Vector2
+
+var tileMap
+var cell
 
 func _ready():
 	direction = Vector2(1,0).rotated(rotation)
+	tileMap = get_parent().get_node("TileMap")
 
 func _physics_process(delta):
 	velocity = SPEED * direction # Gravity so that the bullet falls.
-	# Make bullet disappear if it's in terrain.
-	if is_on_floor() or is_on_wall() or is_on_ceiling():
-		hide()
-
-
-	move_and_slide()
+	# Make bullet disappear if it hits anything
+	collision = move_and_collide(delta * velocity)
+	if collision:
+		# delete the bullet if it hits something
+		queue_free()
+		if collision.get_collider().has_method("hit"):
+			collision.get_collider().hit(collision.get_position() - collision.get_normal())
