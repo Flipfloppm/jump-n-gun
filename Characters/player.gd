64edab0cd7f08f020c2b96cb7d416fc529f477e8
Flipfloppm <franklin.s.dai@gmail.com>
2024-01-23 16:22:38 -0600

    Fixed player pick up weapon interactions, player holding a rocket launcher cannot pick up another rocket launcher

--- Characters/player.gd
+++ Characters/player.gd
@@ -1,12 +1,11 @@
 extends CharacterBody2D
-signal pickedUp
 
 const SPEED = 200.0
 const JUMP_VELOCITY = -300.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-var hasRocketLauncher = false
+var hasRocketLauncher
 var knockingBack = false
 var mousePosVector: Vector2
 var gunRotation
@@ -20,6 +19,7 @@ func _ready():
 	$MultiplayerSynchronizer.set_multiplayer_authority(str(name).to_int())
 	SignalBus.weapon_entered.connect(_on_rocket_body_entered)
 	$GunRotation/RocketLauncher.visible = false
+	hasRocketLauncher = false
 	add_to_group("players")
 	if $MultiplayerSynchronizer.get_multiplayer_authority() == multiplayer.get_unique_id():
 		camera.make_current()
@@ -43,14 +43,7 @@ func _physics_process(delta):
 		elif knockingBack:
 			velocity.x = lerp(velocity.x, 0.0, 0.1)
 		else:
-			#knockingBack = false
 			velocity.x = lerp(velocity.x, 0.0, 0.9)
-		#elif is_on_floor(): # and Input.is_action_just_released("left") or Input.is_action_just_released("right"):
-			##velocity.x = move_toward(velocity.x, 0, SPEED)
-			#if knockingBack:
-				#velocity.x = lerp(velocity.x, 0.0, 0.1)
-			#else:
-				#velocity.x = lerp(velocity.x, 0.0, 0.7)
 			
 		# This rotates the gun following the mouse
 		mousePosVector = Vector2(get_global_mouse_position() - position)
@@ -66,15 +59,13 @@ func _physics_process(delta):
 		move_and_slide()
 
 
-func _on_rocket_body_entered(body):
-	print("body" + str(body) + "self" + str(self))
-	print("entered" + str(multiplayer.get_unique_id()))
+func _on_rocket_body_entered(rocketBody, body):
 	if body != self:
 		return 
-	pickedUp.emit()
-	hasRocketLauncher = true
-	$GunRotation/RocketLauncher.visible = true
-	#$CollisionShape2D.set_deferred("disabled", true)
+	if !hasRocketLauncher: 
+		SignalBus.picked_up.emit(rocketBody)
+		hasRocketLauncher = true
+		$GunRotation/RocketLauncher.visible = true
 	
 
 # pos:	position of explosion
@@ -82,12 +73,9 @@ func _on_rocket_body_entered(body):
 # a:	max explosion force
 # r:	exposion radius
 func on_explosion(pos, b, a, r):
-	
-	print("explode in player")
 	# Find explosion degree and radius.
 	var diff = position - pos # Player position - explosion center position
 	var radius = diff.length()
-	print("radius: " + str(radius))
 	
 	# Knockback if within blast radius
 	if (radius < r):
@@ -96,7 +84,6 @@ func on_explosion(pos, b, a, r):
 		var deg = acos(diff.dot(Vector2(1,0)) / diff.length())
 		if (pos.y > position.y):
 			deg *= -1
-		print("degree: " + str(deg*180/PI))
 		
 		# Calculate force of explosion based on radius
 		var knockback_force = -1 * radius * (a - b) / r + a
@@ -105,11 +92,7 @@ func on_explosion(pos, b, a, r):
 		var knockback_vector = Vector2.ZERO
 		knockback_vector.y = sin(deg) 
 		knockback_vector.x = cos(deg)
-		print("knockback vector: " + str(knockback_vector))
-		print("knockback force: " + str(knockback_force))
 		velocity += knockback_vector * knockback_force
-		print("velocity:" + str(velocity))
-		print()
 		knockback_vector = lerp(knockback_vector, Vector2.ZERO, 0.1)
 	await get_tree().create_timer(0.5).timeout
 	knockingBack = false
--- Items/weapon.gd
+++ Items/weapon.gd
@@ -1,33 +1,18 @@
 extends Area2D
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	SignalBus.picked_up.connect(_on_rocket_launcher_picked_up)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	pass
 
-#func _on_body_entered(body):
-	#print("pistol entered")
-	#hide()
-	#pickedUp.emit()
-	#$CollisionShape2D.set_deferred("disabled", true)
-#
-#
-#func _on_player_picked_up():
-	#queue_free()
-	#$CollisionShape2D.set_deferred("disabled", true)
-
-
 func _on_body_entered(body):
-	print("entered")
+	SignalBus.weapon_entered.emit(self, body)
+
+func _on_rocket_launcher_picked_up(rocketBody):
+	if rocketBody != self:
+		return
 	queue_free()
 	$CollisionShape2D.set_deferred("disabled", true)
-	SignalBus.weapon_entered.emit(body)
-	
-	# This handler replaced the _on_player_picked_up method by using the SignalBus
-#func _on_player_picked_up():
-	
