dfef37d1c07c915f9b12061be23ece2dd0ddf68f
Yimo Wang <yw104@rice.edu>
2024-01-26 02:08:10 -0600

    made spikes and prelimiary code

---
+++ Items/spike.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_area_2d_area_entered(area):
+	get_tree().call_group("players", "die")
+
