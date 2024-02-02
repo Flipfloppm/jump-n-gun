3814f09f74ca0c4e16f4a431c5ae07c18cfe7b03
Yimo Wang <yw104@rice.edu>
2024-02-02 15:52:58 -0600

    made rigid body to have gravity, need to make stick to moving objects

--- Items/c4.gd
+++ Items/c4.gd
@@ -1,11 +1,10 @@
-extends CharacterBody2D
+extends RigidBody2D
 
 const SPEED = 400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var direction : Vector2
-var collision: KinematicCollision2D
 var explosion = preload("res://Items/explosion.tscn")
 var collided = false
 
@@ -13,31 +12,58 @@ var timer: Timer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	direction = Vector2(1,0).rotated(rotation)
+	#direction = Vector2(1,0).rotated(rotation)
+	#linear_velocity = direction * linear_velocity
 	#print("velocity:" + str(linear_velocity))
-	#linear_velocity = Vector2(1,0).rotated(rotation) * speed
+	linear_velocity = Vector2(1,0).rotated(rotation) * SPEED
+	mass = 2
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if !collided:
-		velocity = SPEED * direction
-		collision = move_and_collide(delta * velocity)
-		if collision:
-			collided = true
-			velocity = Vector2.ZERO
-			timer = Timer.new()
-			add_child(timer)
-			timer.wait_time = 3.0
-			timer.one_shot = true
-			timer.connect("timeout", _on_timer_timeout)
-			timer.start()
-	else:
-		velocity = Vector2.ZERO
+	var collision = move_and_collide(linear_velocity * delta)
+	if collision:
+		linear_velocity = linear_velocity.bounce(collision.get_normal())
+		linear_velocity.x *= 0.7
+		linear_velocity.y *= 0.5
+		# Stop moving upon collision.
+		freeze = true
+		collided = true
+	if collided:
+		linear_velocity = Vector2.ZERO
+		timer = Timer.new()
+		add_child(timer)
+		timer.wait_time = 3.0
+		timer.one_shot = true
+		timer.connect("timeout", _on_timer_timeout)
+		timer.start()
 		if Input.is_action_just_pressed("c4detonate"):
 			# Stop timer and explode
 			timer.stop()
 			_on_timer_timeout()
-			
+	#if !collided:
+		##linear_velocity = SPEED * direction
+		#collision = move_and_collide(delta * linear_velocity)
+		#if collision:
+			## Stop moving upon collision.
+			#freeze = true
+			#linear_velocity = linear_velocity.bounce(collision.get_normal())
+			#linear_velocity.x *= 0.7
+			#linear_velocity.y *= 0.5
+			#collided = true
+			#linear_velocity = Vector2.ZERO
+			#timer = Timer.new()
+			#add_child(timer)
+			#timer.wait_time = 3.0
+			#timer.one_shot = true
+			#timer.connect("timeout", _on_timer_timeout)
+			#timer.start()
+	#else:
+		#linear_velocity = Vector2.ZERO
+		#if Input.is_action_just_pressed("c4detonate"):
+			## Stop timer and explode
+			#timer.stop()
+			#_on_timer_timeout()
+			#
 
 
 func _on_timer_timeout():
