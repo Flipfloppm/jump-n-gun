e29a1db35a8ba4b7b874c4e27f4a61e171e50576
Yimo Wang <yw104@rice.edu>
2024-02-12 15:30:17 -0600

    change to break wood only in a radius around impact

--- Levels/multiplayer_level_tilemap.gd
+++ Levels/multiplayer_level_tilemap.gd
@@ -1,85 +1,75 @@
 extends TileMap
 
-var cell
-var allTreeCells = [] # Array of treeCells
-#var treeCells = PackedVector2Array() 
+var breakable_wood_set = {}
+var time_left_set = {}
+# Boundary of playing space
+const XMIN = 0
+const XMAX = 31
+const YMAX = -1
+const YMIN = -55
+const RADIUS = 2
+#var cell
+#var allTreeCells = [] # Array of treeCells
+##var treeCells = PackedVector2Array() 
 var cellSourceId
 var cellAtlasCoords
 var newCellAtlasCoords
-var timer_id_map = {}
-var time_left_id_map = []
+#var timer_id_map = {}
+#var time_left_id_map = []
 
 
 # Called when the node enters the scene tree for the first time.
+# Add all breakable wood indices into the set.
 func _ready():
-	# Add different breakable woods into the big big array.
-	add_to_all_tree_cells(6, 12, -30, -29) # Left 
-	add_to_all_tree_cells(17, 24, -30, -29) # Right
-	add_to_all_tree_cells(13, 16, -37, -36) #top
-
-
-# Adds a range of cells into allTreeCells.
-# xmin, ymin inclusive; xmax, ymax exclusive.
-func add_to_all_tree_cells(xmin, xmax, ymin, ymax):
-	var treeCells = PackedVector2Array()
-	for x in range(xmin, xmax):
-		for y in range(ymin, ymax):
-			var cellCoord = Vector2(x, y)
-			treeCells.append(cellCoord)
-	allTreeCells.append(treeCells)
-	#timer_id_map[allTreeCells.size()-1] = -1 # Temporary placeholder
-	time_left_id_map.append(0)
+	# Add left block.
+	for x in range(6, 12):
+		var cellCoord = Vector2i(x, -30)
+		breakable_wood_set[cellCoord] = 1
+		time_left_set[cellCoord] = 0
+	# Add right block.
+	for x in range(17, 24):
+		var cellCoord = Vector2i(x, -30)
+		breakable_wood_set[cellCoord] = 1
+		time_left_set[cellCoord] = 0
+	# Add top block.
+	for x in range(13, 16):
+		var cellCoord = Vector2i(x, -37)
+		breakable_wood_set[cellCoord] = 1
+		time_left_set[cellCoord] = 0
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	for i in range(0,3):
-		# Countdown time
-		time_left_id_map[i] -= _delta
-		# If time < 0 and tilemap skin is correct skin, reset
-		if time_left_id_map[i] < 0 && get_cell_atlas_coords(0, allTreeCells[i][0]).x> 9.0:
-			for treeCell in allTreeCells[i]:
-				set_cell(0, treeCell, cellSourceId, Vector2(3,5))
-			pass
-	pass
+	for cell in time_left_set:
+		time_left_set[cell] -= _delta
+		if time_left_set[cell] < 0 && get_cell_atlas_coords(0, cell).x > 9.0:
+			cellSourceId = get_cell_source_id(0, cell)
+			set_cell(0, cell, cellSourceId, Vector2(3,5))
+
 
 func hit(collision_position):
-	cell = local_to_map(collision_position)
-	# Go to next image of cell
-	for treeCell_id in range(0, allTreeCells.size()):
-		var treeCells = allTreeCells[treeCell_id]
-		var currAtlas_x
-		if allTreeCells[treeCell_id].has(cell):
-			for treeCell in treeCells:
-				cellSourceId = get_cell_source_id(0, treeCell)
-				cellAtlasCoords = get_cell_atlas_coords(0, treeCell)
-				newCellAtlasCoords = Vector2i(cellAtlasCoords[0] + 3, cellAtlasCoords[1])
-				set_cell(0, treeCell, cellSourceId, newCellAtlasCoords)
-				currAtlas_x = newCellAtlasCoords.x
-			# If block has disappeared, start timer to make block show up again
-			if currAtlas_x > 9.0:
-				time_left_id_map[treeCell_id] = 3.0
-				#var timer = Timer.new()
-				#add_child(timer)
-				#timer_id_map[treeCell_id] = timer.get_instance_id()
-				#print("timer id: ",  timer.get_instance_id())
-				#timer.wait_time = 3.0
-				#timer.one_shot = true
-				#timer.connect("timeout", _on_timer_timeout)
-				#timer.start()
+	var cell = local_to_map(collision_position)
+	# Make sure collision cell is in breakable set.
+	if !(breakable_wood_set.has(cell)):
+		return
+	# Consider an area around initial block of radius r = 3
+	var breaking_set = {}
+	for x_offset in range(-RADIUS, RADIUS+1):
+		for y_offset in range(-RADIUS, RADIUS+1):
+			var other_cell = Vector2i(cell.x + x_offset, cell.y + y_offset)
+			# If cell not in playable space, stop.
+			if other_cell.x < XMIN || other_cell.x > XMAX || other_cell.y < YMIN || other_cell.y > YMAX:
+				continue
+			# Add cell to breaking set.
+			else: 
+				breaking_set[other_cell] = 1
+	# Break blocks
+	for block in breaking_set:
+		cellSourceId = get_cell_source_id(0, block)
+		cellAtlasCoords = get_cell_atlas_coords(0, block)
+		newCellAtlasCoords = Vector2i(cellAtlasCoords[0] + 3, cellAtlasCoords[1])
+		set_cell(0, block, cellSourceId, newCellAtlasCoords)
+		# If broken, change timer.
+		if get_cell_atlas_coords(0, block).x > 9.0:
+			time_left_set[block] = 3.0
 
-
-#func _on_timer_timeout():
-	## Determine which timer has 0 left.
-	#for treeCell_id in timer_id_map:
-		#var timer_id = timer_id_map[treeCell_id]
-		#if timer_id == -1:
-			#return
-		#var timer = instance_from_id(timer_id)
-		#if timer.get_time_left() == 0:
-			#for treeCell in allTreeCells[treeCell_id]:
-				#set_cell(0, treeCell, cellSourceId, Vector2(3,5))
-			## Delete timer
-			#timer.queue_free()
-			## Reset map to -1
-			#timer_id_map[treeCell_id] = -1
