6610504fc64a235050978e1cd8f264e8483ff815
nat hill <wnhill13@gmail.com>
2024-04-05 14:11:56 -0500

    updated hitboxes to 7px

--- Levels/Party Levels/Multiplayer_level2_tilemap.gd
+++ Levels/Party Levels/Multiplayer_level2_tilemap.gd
@@ -1,59 +1,24 @@
 extends TileMap
 
 var breakable_wood_set = {}
-var wood_time_left_set = {}
-const MOSS_TIME = 3.0
-var disappearing_moss_set0 = {}
-var disappearing_moss_set0_timer = MOSS_TIME
-var disappearing_moss_set1 = {}
-var disappearing_moss_set1_timer = 0.0
-
 var tile_spawn_set = {}
-const TILE_SPAWN_TIME = 4.0 # This is the amount of time between image changesad
+const TILE_SPAWN_TIME = 4.0 # This is the amount of time between image changes
 
 # Boundary of playing space
 const XMIN = 0
-const XMAX = 31
-const YMAX = -1
-const YMIN = -144
+const XMAX = 54
+const YMAX = 13
+const YMIN = -27
 const RADIUS = 2
 var cellSourceId
 var cellAtlasCoords
 var newCellAtlasCoords
 
 
+
 # Called when the node enters the scene tree for the first time.
-# Add all breakable wood indices into the set.
 func _ready():
 	SignalBus.tilespawn.connect(spawn_tile_from_gun)
-	## Add cells to breakable wood set. 
-	#add_to_breakable_wood(6, 11, -30, -30)
-	#add_to_breakable_wood(17, 23, -30, -30)
-	#add_to_breakable_wood(13, 15, -37, -37)
-	#add_to_breakable_wood(19, 21, -90, -90)
-	#add_to_breakable_wood(28, 30, -103, -103)
-	#add_to_breakable_wood(1, 5, -111, -111)
-	## Add cells to disappearing moss set.
-	#add_to_disappearing_moss(5, 11, -55, -55, 0)
-	#add_to_disappearing_moss(19, 26, -60, -60, 1)
-	#add_to_disappearing_moss(1, 7, -74, -74, 1)
-	#add_to_disappearing_moss(16, 21, -77, -77, 0)
-	#add_to_disappearing_moss(3, 7, -132, -132, 0)
-	#add_to_disappearing_moss(22, 27, -131, -131, 1)
-
-
-# Helper function to add cells to disappearing moss set.
-func add_to_disappearing_moss(xmin, xmax, ymin, ymax, set_num):
-	if set_num == 0:
-		for x in range(xmin, xmax + 1):
-			for y in range(ymin, ymax + 1):
-				var cellCoord = Vector2i(x, y)
-				disappearing_moss_set0[cellCoord] = 0
-	elif set_num == 1:
-		for x in range(xmin, xmax + 1):
-			for y in range(ymin, ymax + 1):
-				var cellCoord = Vector2i(x, y)
-				disappearing_moss_set1[cellCoord] = 1
 
 
 # Helper function to add cells to breakable wood set.
@@ -62,12 +27,11 @@ func add_to_breakable_wood(xmin, xmax, ymin, ymax):
 		for y in range(ymin, ymax + 1):
 			var cellCoord = Vector2i(x, y)
 			breakable_wood_set[cellCoord] = 1
-			wood_time_left_set[cellCoord] = 0
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-		# For each element of the tile_spawn_set, decrement the time.
+	# For each element of the tile_spawn_set, decrement the time.
 	for cell in tile_spawn_set:
 		tile_spawn_set[cell] -= _delta
 		cellSourceId = get_cell_source_id(0, cell)
@@ -81,27 +45,6 @@ func _process(_delta):
 			if newCellAtlasCoords[0] > 10:
 				tile_spawn_set.erase(cell)
 	pass
-	## Deal with breakable and respawning wood.
-	#for cell in wood_time_left_set:
-		#wood_time_left_set[cell] -= _delta
-		#if wood_time_left_set[cell] < 0 && get_cell_atlas_coords(0, cell).x > 9.0:
-			#cellSourceId = get_cell_source_id(0, cell)
-			#set_cell(0, cell, cellSourceId, Vector2(randi_range(2,3), randi_range(5,6)))
-	## Deal with disappearing moss.
-	#disappearing_moss_set0_timer -= _delta
-	#disappearing_moss_set1_timer -= _delta
-	#if disappearing_moss_set0_timer <= 0:
-		#for cell in disappearing_moss_set0:
-			#cellSourceId = get_cell_source_id(0, cell)
-			#set_cell(0, cell, cellSourceId, Vector2(abs(get_cell_atlas_coords(0, cell).x - 1),0))
-			#disappearing_moss_set0_timer = MOSS_TIME
-	#if disappearing_moss_set1_timer <= 0:
-		#for cell in disappearing_moss_set1:
-			#cellSourceId = get_cell_source_id(0, cell)
-			#set_cell(0, cell, cellSourceId, Vector2(abs(get_cell_atlas_coords(0, cell).x - 1),0))
-			#disappearing_moss_set1_timer = MOSS_TIME
-		#
-
 
 # Script for breaking wood.
 func hit(collision_position):
@@ -129,9 +72,8 @@ func hit(collision_position):
 		cellAtlasCoords = get_cell_atlas_coords(0, block)
 		newCellAtlasCoords = Vector2i(cellAtlasCoords[0] + 3, cellAtlasCoords[1])
 		set_cell(0, block, cellSourceId, newCellAtlasCoords)
-		# If broken, change timer.
-		if get_cell_atlas_coords(0, block).x > 9.0:
-			wood_time_left_set[block] = 3.0
+
+
 
 func spawn_tile_from_gun(pos_x, pos_y):
 	print("tilespawn: " , pos_x, ", ", pos_y)
@@ -142,7 +84,7 @@ func spawn_tile_from_gun(pos_x, pos_y):
 			pass
 		else: 
 			return
-	set_cell(0, cell, 5, Vector2(0,0))
+	set_cell(0, cell, 6, Vector2(0,0))
 	# Add cell to breakable set
 	add_to_breakable_wood(cell.x, cell.x, cell.y, cell.y)
 	# Add cell to tile_spawn_set
